1a8e2d2b8d6eececc21da6270d4270e16be6349c
Matthieu Beauchamp-Boulay <Matthieu_927@hotmail.ca>
2022-09-12 10:38:34 -0400

    finished exercice.py and passed test

--- exercice.py
+++ exercice.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-def majuscule(mot):
-    # TODO completer la fonction ici
-    return mot
+
+def assertChar(char: str) -> None:
+    if len(char) != 1:
+        raise ValueError("expected one character string")
+
+
+def isMin(char: str) -> bool:
+    assertChar(char)
+    # return ord("a") <= ord(char) and ord(char) <= ord("z")
+    return char.islower() # catches accentuated latin letters
+
+
+def toMaj(char: str) -> str:
+    assertChar(char)
+    # return char.upper()
+    return chr(ord(char) - 0x20) if isMin(char) else char
+
+
+def majuscule(mot:str ) -> str:
+    # return mot.upper()
+    chars = [toMaj(char) for char in mot]
+    return "".join(chars)
 
 
 if __name__ == '__main__':
@@ -15,7 +34,12 @@ if __name__ == '__main__':
         'églantier',
         'arbre'
     ]
-    for i in range(len(mots)):
-        mots[i] = majuscule(mots[i])
+
+    for i, mot in enumerate(mots):
+        upper = majuscule(mot)
+        assert upper == mot.upper() 
+
+        mots[i] = upper
+        
 
     print(mots)
